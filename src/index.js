0c9c1cfcaf0504ae9b77f0e401ad3e659783e222
miji_yoon <ymj6337@gmail.com>
2023-07-03 00:17:18 +0900

    ver02

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename='/pethroom'>
         <ScrollToTop />
         <App />
         <Styles />
